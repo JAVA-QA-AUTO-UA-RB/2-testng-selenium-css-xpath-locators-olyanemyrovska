5b915d4d7dc3b8c66fc9b9e6408af76acf5cbc79
olyanemyrovska <110451749+olyanemyrovska@users.noreply.github.com>
2024-08-26 17:36:53 +0200

    Removed unused imports

--- src/test/java/BasicSetupTest.java
+++ src/test/java/BasicSetupTest.java
@@ -1,6 +1,4 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.*;
 
